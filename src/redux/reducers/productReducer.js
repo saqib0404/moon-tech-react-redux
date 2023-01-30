d47f121922f9bcee8678af21549637948f7000f4
Saqib Ahmad <you@example.com>
2023-01-30 21:14:58 +0600

    store setted

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    test: "TEST"
+};
+
+const productReducer = (state = initialState, action) => {
+    return state;
+    // switch (action.type) {
+    //     case :
+
+    //         break;
+
+    //     default:
+    //         break;
+    // }
+}
+
+export default productReducer;
